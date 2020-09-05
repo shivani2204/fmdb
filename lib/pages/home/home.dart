e45b39ec8bb04dfca9f1e3c4faab24404f3030a9
shivani yadav <yshivani1425@gmail.com>
2020-09-05 15:33:35 +0530

    updation

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fmdb/constants/config.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +10,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+ bool isloading = false;
+ String m ="";
+  void fetchMovies() async {
+  setState(() {
+    isloading = true;
+  });
+  var res = await http.get(FMDBConfig.url, headers : {"Authorization": FMDBConfig.accessToken});
+  setState(() {
+    m = res.body;
+  });
+  var decoded = jsonDecode(res.body);
+  }
+  @override
+  void initState() {
+    fetchMovies();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title:Text('My First App')),
-    body: Center(
-      child: Text('https://api.themoviedb.org/3')),
+    body: isloading ? Center(child: CircularProgressIndicator(),) : Center(
+      child: Text(m)
       
+    )
     );
   }
 }
